09e61249e8a6cb91f1b550592eaff7b84da732a5
David-Duf0ur <david.dufour@oclock.school>
2025-07-15 13:35:52 +0200

    add TS for usercontext

--- frontend/src/context/UserContext.tsx
+++ frontend/src/context/UserContext.tsx
@@ -1,5 +1,6 @@
-// @ts-nocheck
+
 import { createContext, useState, type JSX } from "react";
+import type { IUser } from "../types/interfaces";
 
 export const UserContext = createContext({
     user: {
@@ -9,19 +10,19 @@ export const UserContext = createContext({
         email: "",
         idUser: 1,
     },
-    updateUser: (newUser) => { },
+    updateUser: (_newUser: IUser) => { },
 });
 
-export function UserProvider({ children, userContext }): JSX.Element {
+export function UserProvider({ children, userContext }: { children: JSX.Element; userContext: IUser | null }): JSX.Element {
     const [user, setUser] = useState({
         firstname: userContext?.firstname || "Test",
         lastname: userContext?.lastname || "User",
         avatar: userContext?.avatar || 1,
         email: userContext?.email || "test.user@example.com",
-        idUser: userContext?.idUser || 1,
+        idUser: userContext?.id_user || 1,
     });
 
-    const updateUser = (newUser) => {
+    const updateUser = (newUser: IUser) => {
         setUser((prevUser) => ({
             ...prevUser,
             ...newUser,
